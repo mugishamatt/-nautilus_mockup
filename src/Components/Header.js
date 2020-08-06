470766df7faa2249a5f3bda2c657e0a1456a4dc0
mugishamatt <mugisham410@gmail.com>
2020-08-06 01:22:55 -0500

    added links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import{
+    BrowserRouter as Router,
+    Switch,
+    route,
+    Link
+}
+from 'react-router-dom';
 
 
 
 function Header() {
   return (
+      <Fragment>
     <div className="header">
-     
+        <div className="textheader">
+        <p className="issueText"> ISSUE 079</p>
        <p className="title"> Catalyst</p>
-
+       <p className ="agenttext">Agent of change</p>
+       <Router>
+       <Link className="linktext2" to='#'>Read more<br/></Link>
+       <Link className="linktext2" to='https://google.com/'>SEE FULL ISSUE</Link>
+       </Router>
+       </div>
     </div>
-  
+    </Fragment>
    
   )
 
